e88467d9d0cf0595b391ef9c87735d613edc46fd
Shrey <guptashrey163@gmail.com>
2024-11-06 10:55:27 +0530

    Feat: Added user schema

---
+++ src/schema/user.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        minLength:5
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        minLength:5
+       
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength:5
+    }
+},{
+    timestamps:true
+})
+
+const user=mongoose.model("User",userSchema);
+
+export default user;
